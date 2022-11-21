246d0c05d32048a83052d54da2dbe0653cd688db
Abhishek Shukla <abhishek01995@gmail.com>
2022-11-21 18:44:15 +0530

    Routing with multiple modules in angular

--- booStoreApp/src/app/app.module.ts
+++ booStoreApp/src/app/app.module.ts
@@ -2,25 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes =[
-  {path: 'about-us', component: AboutUsComponent},
-  {path: 'how-it-works', component: HowItWorksComponent}
-];
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutUsComponent,
-    HowItWorksComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule,
+    AuthModule
   ],
-  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
