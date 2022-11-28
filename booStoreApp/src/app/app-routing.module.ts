84bdc0b3828a857a15d9047620f0e6847b88b492
Abhishek Shukla <abhishek01995@gmail.com>
2022-11-28 16:57:35 +0530

    44. Auto generate angular material components using schematics

--- booStoreApp/src/app/app-routing.module.ts
+++ booStoreApp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { AuthComponent } from './auth/auth.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about-us', pathMatch: 'full' },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'how-it-works', component: HowItWorksComponent },
-  { path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth-module').then(x => x.AuthModule) },
+  { path: 'auth', component: AuthComponent, loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule) },
+  { path: 'user/:userId', component: UserComponent, loadChildren: () => import('./user/user.module').then(x => x.UserModule) },
   { path: '**', component: NotFoundComponent },
 ];
 
